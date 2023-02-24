1080903b5274aa9f833af84fe98003bc4b68fa4e
Rogisvaldo Js <73315533+nem080@users.noreply.github.com>
2023-02-24 00:44:26 -0300

    aula03

---
+++ aula03/js/script03.js
@@ -0,0 +1,42 @@
+// operadores aula 03 js 
+
+
+// numero inteiro
+let n1 = 10  
+// numero inteiro
+let n2 = 2
+// string
+let n3 = '2'
+
+document.write(`n1 ${n1} <br>`)
+document.write(`n2 ${n2} <br>`)
+document.write(`n3 ${n3} <br>`)
+
+// somando os valores
+
+console.log(n1 + n2) //adição
+console.log(n1 - n2) //subtração
+console.log(n1 * n2) //multiplicaçào
+console.log(n1 / n2) //divisão
+console.log(n1 % n2) // resto da divisão
+
+// sinal de igualdade 
+
+console.log(n2 == n3) //igual a 
+console.log(n2 === n3) //indentico a 
+console.log(n2 != n3) //diferente de não =
+console.log(n2 !== n3) //divisão
+
+// comparações
+console.log(n1 > n2) // maior 
+console.log(n1 < n2) // menor
+console.log(n1 >= n2)// maior ou igual a 
+console.log(n1 <= n2) // menor ou igual a
+
+let a = true
+let b = false
+
+console.log(a && b) // E and
+console.log(a || b) // ou or 
+console.log(!a) // não not
+console.log(!b) // não not
