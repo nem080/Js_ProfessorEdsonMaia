b70edc69633a4ad217d692c7d5150dd4ad9a306e
Rogisvaldo Js <73315533+nem080@users.noreply.github.com>
2023-03-01 20:37:04 -0300

    exec

--- aula06/exec06.js
+++ aula06/exec06.js
@@ -5,4 +5,29 @@ titulo.style.color = '#666';
 
 
 
-document.write('<h2>Dados dos lutador</h2>')
+
+let dadosLutador = document.getElementById('ltdor')
+// Criando um objeto Lutador.
+let lutador = {
+    nome: 'luta nada ',
+    nacionalidade:'Brasolivas',
+    Idade : 35,
+    Peso : 195,
+    Altura : 1.89
+}
+dadosLutador.innerHTML = `<strong><h2>Dados dos lutador</h2></strong><br> Nome: ${lutador.nome}<br> Nacionalidade: ${lutador.nacionalidade}<br> Idade: ${lutador.Idade}<br>Peso: ${lutador.Peso}<br> Altura: ${lutador.Altura}`
+
+let mensgDia = document.querySelector('#msg')
+let dia = 01
+let mes = 03
+let ano = 2023
+let text = 'Ser gênio é algo que não sou, mas o que define meu perfil é ser curioso e sonhador.Todo mundo pode ser inteligente, basta ser curioso e não ter medo de errar.'
+
+mensgDia.innerHTML = `<strong> Estamos no dia ${dia}/${mes}/${ano} </strong>. </br> <h2>Mensagem Do Dia</h2> </br> ${text} <br>`
+
+
+const calendario = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dez'
+]
+
+document.write(calendario)
