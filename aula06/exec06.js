f34a0c747e7516e3aa81b68e5cdc99d696daf2b2
Rogisvaldo Js <73315533+nem080@users.noreply.github.com>
2023-03-01 00:28:41 -0300

    12

---
+++ aula06/exec06.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementById('titulo');
+titulo.innerHTML = 'Exercicios js';
+titulo.style.textAlign = 'center';
+titulo.style.color = '#666';
+
+
+
+document.write('<h2>Dados dos lutador</h2>')
