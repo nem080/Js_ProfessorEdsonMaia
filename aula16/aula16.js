84b8a56be37f1cb0de30c69f0e7a6c48fe2dea6a
Rogisvaldo Js <73315533+nem080@users.noreply.github.com>
2023-03-26 02:45:37 -0300

    feito faltando somente a vincilação dos input ao pontos das jogadas.

--- aula16/aula16.js
+++ aula16/aula16.js
@@ -2,10 +2,33 @@
 
 // SELECIONAR ELEMENTOS
 let numeroSorteado = 0
+let jogaodr01 = document.querySelector('#jogaodr01')
+let jogaodr02 = document.querySelector('#jogaodr02')
 let imgDado = document.querySelector('#imgDado')
 let btnSortear = document.querySelector('#btnSortear')
+let btnlimpar = document.querySelector('#btnlimpar')
 let sorteado = document.querySelector('#sorteado')
 let dadoRolando = document.querySelector('#dadoRolando')
+let respostaDado = document.querySelector('#resp')
+
+
+// verificar o jogaodr
+function selecionarJogador() {
+    let jogadorAtual = jogaodr01
+    if (jogadorAtual === jogaodr01) {
+        jogadorAtual = jogaodr02;
+        return console.log(jogadorAtual);
+    } else {
+        jogadorAtual = jogaodr01;
+        return 1;
+    }
+}
+
+// zerando o dado apos joga-lo
+btnlimpar.addEventListener('click', function(){
+    imgDado.setAttribute('src', '../img/dado/0.png')
+})
+
 
 btnSortear.addEventListener('click', function() {
     // adicionar a animacao
@@ -17,24 +40,25 @@ btnSortear.addEventListener('click', function() {
 
 // ocultar o botao Sortear
     btnSortear.style.display = 'none'
-
+    btnlimpar.style.display ='none'
 // usar setTimeout para executar as acoes apos 1.75 segundos
+
+    function result() {
+        
+    }
     setTimeout(function() {
 
         // ARMAZENAR NUMERO SORTEADO NA VARIAVEL
         numeroSorteado = getRandomInt(1,6)
 
-        // escrever o numero sorteado no console
-        console.log(numeroSorteado)
-
         // definir atributo src com base no numero
-        imgDado.setAttribute('src' , '../images/dado/'+numeroSorteado+'.png')
+        imgDado.setAttribute('src' , '../img/dado/'+numeroSorteado+'.png')
         // escrever no paragrafo numeroSorteado
         sorteado.textContent = numeroSorteado
         
         // exibir o botao Sortear
         btnSortear.style.display = 'inline-block'
-        
+        btnlimpar.style.display = 'inline-block'
         // retirar a animacao
         imgDado.classList.remove('animar')
         sorteado.classList.remove('aparecer')
@@ -49,3 +73,4 @@ function getRandomInt(min, max) {
     max = Math.floor(max) // arredonda para baixo floor = piso
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
